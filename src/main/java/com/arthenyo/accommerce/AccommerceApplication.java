816c60b170f2a8c4c8272628f2f09be1ab96d191
arthenyo <arthenyocarlos19@gmail.com>
2023-05-12 23:43:43 -0300

    Projeto criado

---
+++ src/main/java/com/arthenyo/accommerce/AccommerceApplication.java
@@ -0,0 +1,13 @@
+package com.arthenyo.accommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arthenyo/accommerce/AccommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthenyo.accommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
