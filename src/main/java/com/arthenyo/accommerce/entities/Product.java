b7267da3ee8244a123f7131f024f96a104b6f804
arthenyo <arthenyocarlos19@gmail.com>
2023-05-13 14:05:55 -0300

    OrderItemPK

--- src/main/java/com/arthenyo/accommerce/entities/Product.java
+++ src/main/java/com/arthenyo/accommerce/entities/Product.java
@@ -3,6 +3,7 @@ package com.arthenyo.accommerce.entities;
 import jakarta.persistence.*;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 @Entity
 @Table(name = "tb_product")
@@ -77,4 +78,10 @@ public class Product {
         return categories;
     }
 
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+    public List<Order> getOrders(){
+        return items.stream().map(x -> x.getOrder()).toList();
+    }
 }
