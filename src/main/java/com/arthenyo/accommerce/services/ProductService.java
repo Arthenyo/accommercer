71c2b7ea2ab06c52b83672cd22143a8b0db253c7
Arthenyo <88592884+Arthenyo@users.noreply.github.com>
2023-05-16 11:37:58 -0300

    Inserindo novo produto com POST

--- src/main/java/com/arthenyo/accommerce/services/ProductService.java
+++ src/main/java/com/arthenyo/accommerce/services/ProductService.java
@@ -29,4 +29,17 @@ public class ProductService {
         Page<Product> product = productRepository.findAll(pageable);
         return product.map(x -> new ProductDTO(x));
     }
+
+    @Transactional
+    public ProductDTO insert(ProductDTO dto){
+        Product entity =new Product();
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setPrice(dto.getPrice());
+        entity.setImgUrl(dto.getImgUrl());
+
+        entity = productRepository.save(entity);
+
+        return new ProductDTO(entity);
+    }
 }
