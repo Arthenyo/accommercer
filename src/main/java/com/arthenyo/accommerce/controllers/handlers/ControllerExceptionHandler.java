72ed56736753d94131c3319a6bb18748d38e73e3
Arthenyo <88592884+Arthenyo@users.noreply.github.com>
2023-05-16 15:07:28 -0300

    Customizando a resposta da validação

--- src/main/java/com/arthenyo/accommerce/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/arthenyo/accommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -1,11 +1,14 @@
 package com.arthenyo.accommerce.controllers.handlers;
 
 import com.arthenyo.accommerce.DTO.CustomError;
+import com.arthenyo.accommerce.DTO.ValidationError;
 import com.arthenyo.accommerce.services.excptions.DataBaseExcption;
 import com.arthenyo.accommerce.services.excptions.ResouceNotFoundExcption;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -23,9 +26,19 @@ public class ControllerExceptionHandler {
     }
 
     @ExceptionHandler(DataBaseExcption.class)
-    public ResponseEntity<CustomError> DataBase(DataBaseExcption e, HttpServletRequest request) {
+    public ResponseEntity<CustomError> dataBase(DataBaseExcption e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.BAD_REQUEST;
         CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomError> methodArgumentNotValid(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError err = new ValidationError(Instant.now(), status.value(), "Dados invalido", request.getRequestURI());
+        for(FieldError f : e.getBindingResult().getFieldErrors()){
+            err.addError(f.getField(),f.getDefaultMessage());
+        }
         return ResponseEntity.status(status).body(err);
     }
 }
