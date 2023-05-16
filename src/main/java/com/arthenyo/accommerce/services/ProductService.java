210e746781c4feeb06b40dd3461c7f0dc4c40da9
Arthenyo <88592884+Arthenyo@users.noreply.github.com>
2023-05-16 13:14:31 -0300

    Atualizando produto com PUT

--- src/main/java/com/arthenyo/accommerce/services/ProductService.java
+++ src/main/java/com/arthenyo/accommerce/services/ProductService.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 import java.util.Optional;
@@ -33,13 +34,24 @@ public class ProductService {
     @Transactional
     public ProductDTO insert(ProductDTO dto){
         Product entity =new Product();
+        copyDtoToEntity(dto, entity);
+        entity = productRepository.save(entity);
+        return new ProductDTO(entity);
+    }
+
+    @Transactional
+    public ProductDTO update(Long id, ProductDTO dto){
+        Product entity = productRepository.getReferenceById(id);
+        copyDtoToEntity(dto, entity);
+        entity = productRepository.save(entity);
+        return new ProductDTO(entity);
+    }
+
+    private void copyDtoToEntity(ProductDTO dto, Product entity) {
+
         entity.setName(dto.getName());
         entity.setDescription(dto.getDescription());
         entity.setPrice(dto.getPrice());
         entity.setImgUrl(dto.getImgUrl());
-
-        entity = productRepository.save(entity);
-
-        return new ProductDTO(entity);
     }
 }
